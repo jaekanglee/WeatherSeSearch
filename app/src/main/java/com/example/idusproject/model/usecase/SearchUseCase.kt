eded3bb51449c399c80db67acfb587570d702e44
ppizil <ksdy234@gmail.com>
2021-01-18 20:46:21 +0900

    Update [ Usecase ] - 각 api에 맞게 Usecase 분리 - SingleUseCase를 추상 클래스로 구현함으로써 , 재사용성이 용이하도록 변경

--- app/src/main/java/com/example/idusproject/model/usecase/SearchUseCase.kt
+++ app/src/main/java/com/example/idusproject/model/usecase/SearchUseCase.kt
@@ -1,16 +1,14 @@
 package com.example.idusproject.model.usecase
 
 import com.example.idusproject.model.remote.Repository
-import com.example.idusproject.model.remote.Result
-import com.example.idusproject.model.remote.RepositoryImpl
-import com.example.idusproject.model.remote.entity.SearchWordEntity
+import com.example.idusproject.model.remote.entity.Location
 import io.reactivex.Single
 
 class SearchUseCase(val searchApi: Repository) :
-    SingleUseCase<String, List<SearchWordEntity>>() {
+    SingleUseCase<String, List<Location>>() {
 
 
-    override fun execute(parameter: String): Single<Result<List<SearchWordEntity>>> {
+    override fun execute(parameter: String): Single<List<Location>> {
         return searchApi.getSearchWord(parameter)
     }
 }
--- app/src/main/java/com/example/idusproject/model/usecase/SingleUseCase.kt
+++ app/src/main/java/com/example/idusproject/model/usecase/SingleUseCase.kt
@@ -4,24 +4,17 @@ import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.Single
 import io.reactivex.disposables.Disposable
-import com.example.idusproject.model.remote.Result
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers.io
 
 abstract class SingleUseCase<T, R>() {
-    private val result = MutableLiveData<Result<R>>()
-    fun observe() = result
+
 
     @SuppressLint("CheckResult")
-    operator fun invoke(parameter: T): Disposable {
+    operator fun invoke(parameter: T): Single<R> {
         return execute(parameter).subscribeOn(io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                result.value = it //여기서 왜 반응을 안하 지
-            }, {
-                result.value = Result.Error("server")
-            })
     }
 
-    abstract fun execute(parameter: T): Single<Result<R>>
+    abstract fun execute(parameter: T): Single<R>
 }
